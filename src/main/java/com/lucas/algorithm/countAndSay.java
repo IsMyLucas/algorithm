ccd9c77b08b569dddabf573ad1657dea4c642e23
林东鑫 <1095939818@qq.com>
2021-01-27 10:03:41 +0800

    leetcode38
    Count and Say

---
+++ src/main/java/com/lucas/algorithm/countAndSay.java
@@ -0,0 +1,96 @@
+package com.lucas.algorithm;
+
+public class countAndSay {
+
+    /**
+     * 38. Count and Say
+     *
+     * he count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+     *
+     * countAndSay(1) = "1"
+     * countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+     * To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character.
+     * Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and
+     * concatenate every saying.
+     * For example, the saying and conversion for digit string "3322251":
+     * Given a positive integer n, return the nth term of the count-and-say sequence.
+     *
+     *
+     * Example 1:
+     * Input: n = 1
+     * Output: "1"
+     * Explanation: This is the base case.
+     *
+     * Example 2:
+     * Input: n = 4
+     * Output: "1211"
+     * Explanation:
+     * countAndSay(1) = "1"
+     * countAndSay(2) = say "1" = one 1 = "11"
+     * countAndSay(3) = say "11" = two 1's = "21"
+     * countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+     */
+
+    /**
+     * 计数和演讲，时间复杂度大于O(n),小于O(n^2)，空间复杂度O(n)
+     * @param n
+     * @return
+     */
+    public static String countAndSay(int n) {
+        if(n<=0) return "";
+        String res = "1";
+        char say;
+        for (int i=1;i<n;i++){
+            char[] c = res.toCharArray();
+            res = "";
+            int j = 1,count=1;
+            say = c[0];
+            while (j<c.length) {
+                if(c[j] != say){
+                    res = res + count + say;
+                    count = 1;
+                    say = c[j];
+                }else{
+                    count++;
+                }
+                j++;
+            }
+            res = res + count + say;
+        }
+        return res;
+    }
+
+    public static String countAndSay1(int n) {
+        StringBuilder curr=new StringBuilder("1");
+        StringBuilder prev;
+        int count;
+        char say;
+        for (int i=1;i<n;i++){
+            prev=curr;
+            curr=new StringBuilder();
+            count=1;
+            say=prev.charAt(0);
+
+            for (int j=1,len=prev.length();j<len;j++){
+                if (prev.charAt(j)!=say){
+                    curr.append(count).append(say);
+                    count=1;
+                    say=prev.charAt(j);
+                }
+                else count++;
+            }
+            curr.append(count).append(say);
+        }
+        return curr.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countAndSay(1));//1
+        System.out.println(countAndSay(2));//11
+        System.out.println(countAndSay(3));//21
+        System.out.println(countAndSay(4));//1211
+        System.out.println(countAndSay(5));//111221
+        System.out.println(countAndSay(6));//312211
+    }
+
+}

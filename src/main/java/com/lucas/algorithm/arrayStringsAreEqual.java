638e578618f660929fbf7a798831804bd56ef8d1
林东鑫 <1095939818@qq.com>
2021-06-02 18:12:03 +0800

    leetcode938&1486&1614&1773&1832&1221&1662
    1662. Check If Two String Arrays are Equivalent
    1221. Split a String in Balanced Strings
    1832. Check if the Sentence Is Pangram
    1773. Count Items Matching a Rule
    1614. Maximum Nesting Depth of the Parentheses
    1486. XOR Operation in an Array
    938. Range Sum of BST

---
+++ src/main/java/com/lucas/algorithm/arrayStringsAreEqual.java
@@ -0,0 +1,37 @@
+package com.lucas.algorithm;
+
+public class arrayStringsAreEqual {
+    /**
+     * 1662. Check If Two String Arrays are Equivalent
+     * Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+     * A string is represented by an array if the array elements concatenated in order forms the string.
+     *
+     * Example 1:
+     * Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+     * Output: true
+     * Explanation:
+     * word1 represents string "ab" + "c" -> "abc"
+     * word2 represents string "a" + "bc" -> "abc"
+     * The strings are the same, so return true.
+     *
+     * Example 2:
+     * Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+     * Output: false
+     *
+     * Example 3:
+     * Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+     * Output: true
+     */
+
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder string1 = new StringBuilder();
+        StringBuilder string2 = new StringBuilder();
+        for (String str:word1) {
+            string1.append(str);
+        }
+        for (String str:word2) {
+            string2.append(str);
+        }
+        return string1.toString().equals(string2.toString());
+    }
+}
---
+++ src/main/java/com/lucas/algorithm/checkIfPangram.java
@@ -0,0 +1,34 @@
+package com.lucas.algorithm;
+
+public class checkIfPangram {
+    /**
+     * 1832. Check if the Sentence Is Pangram
+     * A pangram is a sentence where every letter of the English alphabet appears at least once.
+     * Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+     *
+     * Example 1:
+     * Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+     * Output: true
+     * Explanation: sentence contains at least one of every letter of the English alphabet.
+     *
+     * Example 2:
+     * Input: sentence = "leetcode"
+     * Output: false
+     */
+
+    public boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) return false;
+        boolean res[] = new boolean[26];
+        int i = 0;
+        for (char c:sentence.toCharArray()) {
+            if (res [c-97] == false){
+                res [c-97] = true;
+                i++;
+            }
+        }
+        if (i < 26){
+            return false;
+        }
+        return true;
+    }
+}
